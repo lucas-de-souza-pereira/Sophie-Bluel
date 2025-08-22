3503a7e1c5c330f30cb8c9af46b4c4d756b26c4b
lucas-de-souza-pereira <lucas.de-souza-pereira@laplateforme.io>
2025-08-22 14:39:51 +0200

    add editlink into section portfolio

--- FrontEnd/assets/src/pages/home_connected.js
+++ FrontEnd/assets/src/pages/home_connected.js
@@ -6,6 +6,8 @@ export async function connectedHome() {
 
 function renderConnected(){
     editionBar()
+    renderLogout()
+    renderEditGallery()
 }
 
 
@@ -27,4 +29,35 @@ function editionBar(){
     editionBar.appendChild(editionElement)
 
     bodyElement.insertBefore(editionBar, bodyElement.firstChild)
+}
+
+function renderLogout(){
+
+    const logoutElement = document.getElementById("nav-login")
+    logoutElement.classList.add("logout")
+    logoutElement.href = "index.html"
+    logoutElement.innerText = "logout"
+
+    logoutElement.addEventListener("click", () =>{
+
+        localStorage.removeItem("token");
+        
+    })
+}
+
+function renderEditGallery(){
+
+    const portfolioElement = document.getElementById("portfolio")
+
+    const divPortfolio = document.createElement("div")
+    divPortfolio.classList.add("portfiolio-connected")
+
+    const h2Portfolio = document.querySelector("#portfolio h2")
+
+    const editLink = document.createElement("a")
+    editLink.innerHTML = "<i class='fa-regular fa-pen-to-square'></i> modifier"
+
+    portfolioElement.insertBefore(divPortfolio,h2Portfolio)
+    divPortfolio.appendChild(h2Portfolio)
+    divPortfolio.appendChild(editLink)
 }
